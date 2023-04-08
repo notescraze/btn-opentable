3de8e7f4dbe2f509bbe74517b8a4b8f52c2ffb1e
notescraze <serenetechqqo24@gmail.com>
2023-04-08 08:39:20 +0800

    new changes

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-dark">
+        {/* NavBAR */}
+
+        {/* NavBar */}
+      </main>
+    </main>
+  );
+}
